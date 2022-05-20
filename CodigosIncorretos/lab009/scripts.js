53bce205be6e1fe101efa76f1ded5926e02419e5
Natália Santana <nataliahie225@gmail.com>
2022-05-20 11:02:22 -0300

    aAtualizando o lab 09

--- CodigosIncorretos/lab009/scripts.js
+++ CodigosIncorretos/lab009/scripts.js
@@ -3,12 +3,14 @@ let contador = 0
 let res = document.querySelector('section#result')
 // As variáveis declaradas aqui fora são consideradas GLOBAIS e funcionam dentro do programa inteiro
 
+
+//Atualizei o contador para ++
 function contar() {
-    contador -- // É a mesma coisa que contador = contador + 1
+    contador ++ // É a mesma coisa que contador = contador + 1
     res.innerHTML = `<p>O contador está com <mark>${contador}</mark> cliques.</p>`
 }
-
+//Atualizei para contador  = 0
 function zerar() {
-    contador
+    contador = 0
     res.innerHTML = null
 }
