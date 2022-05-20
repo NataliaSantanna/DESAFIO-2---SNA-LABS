d78a0244d361e67460930ced2671fa5f8bc36240
Natália Santana <nataliahie225@gmail.com>
2022-05-20 18:14:01 -0300

    commit

--- DesafiosFinais 2/desafio-conversor-monetario/index.js
+++ DesafiosFinais 2/desafio-conversor-monetario/index.js
@@ -1,27 +1,17 @@
+let converter = document.getElementById('Converter')
 function Converter(){
     var dolar =  window.prompt('Antes de mais nada. Quanto está a cotação do dólar agora?')
     var reais =  window.prompt('Quantos reais você tem na carteira?')
     
-    let converter = document.getElementById('Converter')
     
-    let dolar= 0,20
-    let real =  4,88
-    let decametro = metros / 10
-    let decimetros = metros * 10
-    let centimetros = metros * 100
-    let milimetros = metros * 1000
+    
+    let divisao = reais / dolar
     
     
     
-    converter.innerHTML = `<h2> A distancia de ${metros} é corresponde a : </p>`
-    converter.innerHTML += `<p> ${kilometro} quilômetros (km) </p>`
-    converter.innerHTML += `<p> ${hectometro} hectometros(hm) </p>`
-    converter.innerHTML += `<p> ${decametro}  decametros(dam) </p>`
-    converter.innerHTML += `<p> ${decimetros} decimetros (dm) </p>`
-    converter.innerHTML += `<p> ${centimetros} centimetros (cm) </p>`
-    converter.innerHTML += `<p> ${milimetros}  milimetros (ml) </p>`
-    
-    
+    converter.innerHTML = `<h2> A coversão é de ${divisao}  </p>`
+  
+   
     
     
     }
