ac38c2b83444718539bf71e534c8a67b85a47e1a
Natália Santana <nataliahie225@gmail.com>
2022-05-20 11:36:31 -0300

    Atualiando o lab 14

--- CodigosIncorretos/lab014/functions.js
+++ CodigosIncorretos/lab014/functions.js
@@ -1,5 +1,6 @@
 
-function info[] {
+//Alterei o colchetes para parenteses.
+function info() {
     let agora = new Date
     let saida = document.getElementById('saida')
     saida.innerHTML = `<p>O que eu recebi do sistema foi <mark>${agora}</mark></p>`
