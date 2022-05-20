816144cbd45df622de70a70c17a1d9280bab2796
Natália Santana <nataliahie225@gmail.com>
2022-05-20 11:28:00 -0300

    Atualizando o lab 12

--- CodigosIncorretos/lab012/parimpar.js
+++ CodigosIncorretos/lab012/parimpar.js
@@ -2,12 +2,13 @@
 function teste() {
     let num = Number(window.prompt('Digite um número: '))
     let tipo
-    if (num % 2 == 1) {
+    //A logica estava errada, pois para ser par o resto da divisao deve ser igual a 0 e nao 1
+    if (num % 2 == 0) {
         tipo = '<strong>PAR</strong>'
     } else {
         tipo = '<strong>ÍMPAR</strong>'
     }
-
-    let res = querySelector('section#result')
+//Adicionei o "document" em querySlector
+    let res = document.querySelector('section#result')
     res.innerHTML = `<p>O número ${num} que foi digitado é ${tipo}!</p>`
 }
