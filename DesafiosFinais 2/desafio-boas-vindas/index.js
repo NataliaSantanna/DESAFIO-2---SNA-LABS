53eaf7b67b1aba198796a7f0189a0b976630f131
Natália Santana <nataliahie225@gmail.com>
2022-05-20 14:44:33 -0300

    Finalizando ex 01

---
+++ DesafiosFinais 2/desafio-boas-vindas/index.js
@@ -0,0 +1,6 @@
+function Iniciar(){
+    let nome = window.prompt('Qual seu nome?')
+    let idade = window.prompt(`Òlá ${nome}!Quantos anos você tem?`)
+    alert(`Acabei de conhecer ${nome} que tem ${idade} anos de idade`)
+}
+
